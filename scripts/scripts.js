ad19cfc7530dfc778c7ccab4f39f43e84a69605c
Richard <richardzehungho@gmail.com>
2025-08-13 21:39:53 -0700

    add styles for landing mobile section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,52 @@
+const symbol = document.querySelector(".symbol-h1");
+const nav = document.querySelector("nav");
+const symbolHome = document.querySelector(".symbol-home");
+
+window.addEventListener("scroll", () => {
+  let scrollPos = window.scrollY;
+
+  console.log(scrollPos);
+
+  // makes it scroll up when the user scrolls down
+  symbol.style.transform = `translateY(${scrollPos * -0.75}px)`;
+
+  if (scrollPos >= 100) {
+    console.log("i am true");
+    nav.classList.add("nav-active");
+    symbolHome.style.opacity = 1;
+  } else {
+    nav.classList.remove("nav-active");
+    symbolHome.style.opacity = 0;
+  }
+});
 
 // Hide the radio buttons on site load
 
 // Radio buttons slide up on hover
-const radioBtn = document.getElementsByName('item-color');
+const radioBtn = document.getElementsByName("item-color");
 radioBtn.forEach(function (radio) {
-    radio.addEventListener('change', function(e) {
-        const target = e.currentTarget;
-        const color = target.value;
+  radio.addEventListener("change", function (e) {
+    const target = e.currentTarget;
+    const color = target.value;
 
-        const gridItem = target.closest('.grid-item');
-        const gridImg = gridItem.querySelector('img');
+    const gridItem = target.closest(".grid-item");
+    const gridImg = gridItem.querySelector("img");
 
-        gridImg.src = this.dataset.img;
+    gridImg.src = this.dataset.img;
+  });
+});
 
-    })
-})
-
-const images =document.getElementsByClassName("image");
-
+const images = document.getElementsByClassName("image");
 
 for (let i = 0; i < images.length; i++) {
-    const imageNames= document.getElementsByTagName("h5");
-    const picture= document.getElementsByTagName("img");
-  images[i].addEventListener("mouseover", function() {
+  const imageNames = document.getElementsByTagName("h5");
+  const picture = document.getElementsByTagName("img");
+  images[i].addEventListener("mouseover", function () {
     // imageNames[i].style.textDecoration = "underline";
     picture[i].style.opacity = "0.5";
-
   });
 
-  images[i].addEventListener("mouseout", function() {
+  images[i].addEventListener("mouseout", function () {
     // imageNames[i].style.textDecoration = "none";
     picture[i].style.opacity = "1";
   });
